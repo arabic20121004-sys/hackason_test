17c0a324ff2bb5d256c39cf2664ed5329368c6f6
lishao <arabic20121004@gmail.com>
2025-12-29 16:21:35 +0900

    不要なprint関数を削除

--- example.py
+++ example.py
@@ -8,12 +8,9 @@ greet = print("こんにちは")
 def print_name(name):
     print(f"私の名前は{name}です")
 
-print(print_name("太郎"))
-
 #4.「おはようございます」という文字列を戻り値として返すget_greet関数を実行し、戻り値をprint関数で出力
 def get_greet():
       return f"おはようございます"
-
 print(get_greet())
 
 #5.a,bを引数に取り、その足し算を戻り値として返すadd関数を実装し、関数を呼び出して結果をprint関数で出力
